924d73a94d704c7dfa779c92fd530e2c56f15056
John Leonoras <john.emberga.leonoras@gmail.com>
2022-10-10 22:25:12 +0800

    --

--- mini-exercises/plus-minus-button/main.js
+++ mini-exercises/plus-minus-button/main.js
@@ -1,33 +1,33 @@
 function displayDafaultValue() {
-    let x = 0
-    return x;
+  let x = 0;
+  return x;
 }
 
 let defaultValue = displayDafaultValue();
 document.getElementById("output-area").innerHTML = defaultValue;
 
 function decreaseValue() {
-    defaultValue = defaultValue - 1;
-    document.getElementById("output-area").innerHTML = defaultValue;
+  defaultValue = defaultValue - 1;
+  document.getElementById("output-area").innerHTML = defaultValue;
 }
 
 function increaseValue() {
-    defaultValue = defaultValue + 1;
-    document.getElementById("output-area").innerHTML = defaultValue;
+  defaultValue = defaultValue + 1;
+  document.getElementById("output-area").innerHTML = defaultValue;
 }
 
 function resetValue() {
-    defaultValue = 0;
-    document.getElementById("output-area").innerHTML = defaultValue;
+  defaultValue = 0;
+  document.getElementById("output-area").innerHTML = defaultValue;
 }
 
 function displayLabel(sign) {
-    return sign
+  return sign;
 }
 
-const decreaseSign = displayLabel("-")
-const increaseSign = displayLabel("+")
-const resetLabel = displayLabel("Reset")
+const decreaseSign = displayLabel("-");
+const increaseSign = displayLabel("+");
+const resetLabel = displayLabel("Reset");
 
 document.getElementById("decreaseLabel").innerHTML = decreaseSign;
 document.getElementById("increaseLabel").innerHTML = increaseSign;
