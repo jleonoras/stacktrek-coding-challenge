d6a868ab595a43041f03c9ffc7fedbde766c6ed8
John Leonoras <john.emberga.leonoras@gmail.com>
2022-10-11 11:06:16 +0800

    ...

--- mini-exercises/site-project/script.js
+++ mini-exercises/site-project/script.js
@@ -24,18 +24,10 @@ const displayInfo = () => {
   document.getElementById("myName").innerHTML = xName;
   document.getElementById("myCareer").innerHTML = xCareer;
 
-  // document.getElementById("hobbies").innerHTML = myProfile.profileList[0];
-  // document.getElementById("favFoods").innerHTML = myProfile.profileList[1];
-  // document.getElementById("favSeries").innerHTML = myProfile.profileList[2];
-  // document.getElementById("favMovies").innerHTML = myProfile.profileList[3];
-
-  document.getElementById("hobbies").innerHTML = myProfile.profileList.hobbies;
-  document.getElementById("favFoods").innerHTML =
-    myProfile.profileList.favFoods;
-  document.getElementById("favSeries").innerHTML =
-    myProfile.profileList.favSeries;
-  document.getElementById("favMovies").innerHTML =
-    myProfile.profileList.faveMovies;
+  document.getElementById("hobbies").innerHTML = myProfile.profileList[0];
+  document.getElementById("favFoods").innerHTML = myProfile.profileList[1];
+  document.getElementById("favSeries").innerHTML = myProfile.profileList[2];
+  document.getElementById("favMovies").innerHTML = myProfile.profileList[3];
 };
 
 const makeUL = (array) => {
